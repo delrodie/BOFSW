32dac87e16c32f8f7dd8328d11471962e6567442
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2017-07-25 14:41:30 +0000

    Mise en page de la classe Presentation

--- src/AppBundle/Entity/Presentation.php
+++ src/AppBundle/Entity/Presentation.php
@@ -56,7 +56,7 @@ class Presentation
     /**
      * NOTE: This is not a mapped field of entity metadata, just a simple property.
      *
-     * @Vich\UploadableField(mapping="base_image", fileNameProperty="imageName", size="imageSize")
+     * @Vich\UploadableField(mapping="presentation_image", fileNameProperty="imageName", size="imageSize")
      *
      * @var File
      */
@@ -86,8 +86,8 @@ class Presentation
     /**
      * @var string
      *
-     * @Gedmo\Slug(fields={"libelle"})
-     * @ORM\Column(name="slug", type="string", length=75)
+     * @Gedmo\Slug(fields={"titre"})
+     * @ORM\Column(name="slug", type="string", length=255)
      */
     private $slug;
 
@@ -420,5 +420,37 @@ class Presentation
     public function getModifieLe()
     {
         return $this->modifieLe;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     *
+     * @return Product
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
     }
 }
